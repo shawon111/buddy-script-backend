aac34ba11d43a910419567f2544cfe6f2ece40cf
shawon111 <shawonahmmed08509@gmail.com>
2026-04-05 23:19:06 +0600

    cookie settings change v3

--- src/utils/setAuthCookie.js
+++ src/utils/setAuthCookie.js
@@ -2,7 +2,6 @@ const setAuthCookie = (res, cookieType, tokens, age) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: 'none',
         maxAge: age
     };
     res.cookie(cookieType, tokens[cookieType], options);
